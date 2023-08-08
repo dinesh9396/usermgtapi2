2b1c8ed9ffe1c065183a2699ddaf30f62729b186
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2023-08-08 11:16:30 +0530

    New

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
+from dotenv import load_dotenv
 from pymongo import ReturnDocument
 import bcrypt
 import jwt
 import datetime
+import os
 
+load_dotenv()
 app = Flask(__name__)
 # Replace with your MongoDB URI
-app.config['MONGO_URI'] = "mongodb+srv://mldinesh0:DPGq3M5xFkYGKfD6@cluster0.mnugi39.mongodb.net/indian_hacker_news?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true"
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 mongo = PyMongo(app)
 
 
 def generate_token(user, secret_key, hours_to_expire=1):
     # Calculate the expiration time
-    expiration_time = datetime.datetime.utcnow(
-    ) + datetime.timedelta(hours=hours_to_expire)
+    expiration_time = datetime.datetime.utcnow() + datetime.timedelta(hours=hours_to_expire)
 
     payload = {
         'id': str(user['_id']),
@@ -24,11 +26,12 @@ def generate_token(user, secret_key, hours_to_expire=1):
         'license': user['license'],
         'diaryblogAccess': user.get('diaryblogAccess'),
         'typeitAccess': user.get('typeitAccess'),
-        'expiration_time':  expiration_time
+        'expiration_time': expiration_time.strftime('%Y-%m-%d %H:%M:%S')  # Convert to string
     }
 
     token = jwt.encode(payload, secret_key, algorithm='HS256')
     return token
+
 
 
 def is_token_expired(token, secret_key):
@@ -74,14 +77,32 @@ def register_user():
 
     return jsonify({'message': 'User registered successfully', 'user_id': str(user_id)}), 201
 
+@app.route('/api/login', methods=['POST'])
+def login_user():
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
+
+    user = mongo.db.users.find_one({'email': email})
+
+    if not user or not bcrypt.checkpw(password.encode('utf-8'), user['password']):
+        return jsonify({'message': 'Invalid credentials'}), 401
+
+    # Define your actual secret key here
+    secret_key = 'Welcome'
+
+    token = generate_token(user, secret_key)
+    return jsonify({'message': 'Login successful', 'token': token}), 200
+
+
 
 @app.route('/api/menuOption', methods=['POST'])
 def menuOption():
-    # add logic to fetch token from header and extract the data needed to be added
+    # Add logic to fetch token from header and extract the data needed to be added
     secret_key = 'YourSecretKeyHere'
-    token = is_token_expired(token, secret_key)
-    return jsonify({'message': 'Login successful', 'token': token}), 200
-
+    token = request.headers.get('Authorization')  # Extract token from header
+    token_expired = is_token_expired(token, secret_key)
+    return jsonify({'message': 'Token verification successful', 'token_expired': token_expired}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
