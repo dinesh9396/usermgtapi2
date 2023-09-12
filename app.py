20ea304ee9240de566265595be43e29c2fb1077f
VINOTH <103953710+VinothManickam@users.noreply.github.com>
2023-09-12 13:18:05 +0530

    new add

--- app.py
+++ app.py
@@ -1,124 +1,81 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 from flask_pymongo import PyMongo
-from flask_cors import CORS
-from dotenv import load_dotenv
-from pymongo import ReturnDocument
-from bson import json_util
-from flask import Response
+from bson import ObjectId
 import bcrypt
-import jwt
-import datetime
 import os
+from dotenv import load_dotenv
+import jwt
+import datetime  # Import the datetime module
 
+# Load environment variables from the .env file
 load_dotenv()
+
 app = Flask(__name__)
-CORS(app)
-# Replace with your MongoDB URI
-app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+
+# Configure MongoDB connection
+app.config['MONGO_URI'] = os.getenv("MONGO_URI")
 mongo = PyMongo(app)
-secret_key = os.environ.get('SECRET_KEY')
+users_collection = mongo.db.users
 
+# Secret key for session management
+app.secret_key = os.getenv("SECRET_KEY")
 
-def generate_token(user, secret_key, hours_to_expire=1):
-    # Calculate the expiration time
-    expiration_time = datetime.datetime.utcnow() + datetime.timedelta(hours=hours_to_expire)
+jwt_secret_key = os.getenv("JWT_SECRET_KEY")
 
-    payload = {
-        'id': str(user['_id']),
-        'name': user['name'],
-        'email': user['email'],
-        'profession': user['profession'],
-        'license': user['license'],
-        'diaryblogAccess': user.get('diaryblogAccess'),
-        'typeitAccess': user.get('typeitAccess'),
-        'exp': expiration_time  # Use 'exp' claim for expiration
-    }
+if jwt_secret_key is None or not isinstance(jwt_secret_key, str):
+    raise ValueError("JWT_SECRET_KEY is not set or is not a string")
 
-    token = jwt.encode(payload, secret_key, algorithm='HS256')
-    return token
-
-
-
-def is_token_expired(token, secret_key):
-    try:
-        decoded_payload = jwt.decode(
-            token, secret_key, algorithms=['HS256'])
-        expiration_time = datetime.datetime.fromtimestamp(
-            decoded_payload['exp'])
-        current_time = datetime.datetime.utcnow()
-        return current_time > expiration_time
-    except jwt.ExpiredSignatureError:
-        return True
-    except jwt.InvalidTokenError:
-        return True
-
-
+# Endpoint for user registration
 @app.route('/api/register', methods=['POST'])
-def register_user():
+def register():
     data = request.json
-    name = data.get('name')
-    email = data.get('email')
-    password = data.get('password')
-    profession = data.get('profession')
-    license = data.get('license')
-    diaryblog_access = data.get('diaryblogAccess')
-    typeit_access = data.get('typeitAccess')
 
-    existing_user = mongo.db.users.find_one({'email': email})
+    # Check if email is already registered
+    existing_user = users_collection.find_one({"email": data["email"]})
     if existing_user:
-        return jsonify({'message': 'User already exists'}), 400
+        return jsonify({"message": "Email already registered"}), 400
 
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    # Hash the password securely
+    hashed_password = bcrypt.hashpw(data["password"].encode('utf-8'), bcrypt.gensalt())
 
-    user = mongo.db.users.insert_one({
-        'name': name,
-        'email': email,
-        'password': hashed_password,
-        'profession': profession,
-        'license': license,
-        'diaryblogAccess': diaryblog_access,
-        'typeitAccess': typeit_access,
+    # Create a new user document with corrected fields
+    user_id = mongo.db.users.insert_one({
+        "email": data["email"],
+        "username": data["username"],
+        "password": hashed_password,
+        "createDate": datetime.datetime.utcnow(),
+        "updateDate": datetime.datetime.utcnow(),
+        "diaryblogAccess": data.get('diaryblogAccess'),  # Corrected line
+        "typeitAccess": data.get('typeitAccess')  # Corrected line
     })
 
-    user_id = user.inserted_id
+    return jsonify({"message": "Registration successful"}), 201
 
-    user_data = mongo.db.users.find_one({'_id': user_id})
-
-    secret_key = os.environ.get('SECRET_KEY')
-
-    token = generate_token(user_data, secret_key)
-
-    data = json_util.dumps({'message': 'User registered successfully', 'user_id': str(user_id), 'token': token, 'user': user_data})
-    return Response(data, mimetype='application/json'), 201
-
+# Endpoint for user login
 @app.route('/api/login', methods=['POST'])
-def login_user():
+def login():
     data = request.json
-    email = data.get('email')
-    password = data.get('password')
+    user = users_collection.find_one({"email": data["email"]})
 
-    user = mongo.db.users.find_one({'email': email})
+    if user:
+        # Check the hashed password
+        if bcrypt.checkpw(data["password"].encode('utf-8'), user["password"]):
+            # Generate a JWT token upon successful login
+            token_payload = {
+                "user_id": str(user["_id"]),
+                "email": user["email"],
+                "username": user["username"]
+            }
+            token = jwt.encode(token_payload, jwt_secret_key, algorithm='HS256')
 
-    if not user or not bcrypt.checkpw(password.encode('utf-8'), user['password']):
-        return jsonify({'message': 'Invalid credentials'}), 401
+            # Return the JWT token as a response
+            return jsonify({"message": "Login successful", "token": token}), 200
+        else:
+            return jsonify({"message": "Incorrect password"}), 401
+    else:
+        return jsonify({"message": "User not found"}), 404
 
-    # Define your actual secret key here
-    secret_key = os.environ.get('SECRET_KEY')
-
-    token = generate_token(user, secret_key)
-    data = json_util.dumps({'message': 'Login successful', 'token': token, 'user': user})
-    return Response(data, mimetype='application/json'), 200
-
-
-
-
-@app.route('/api/menuOption', methods=['POST'])
-def menuOption():
-    # Add logic to fetch token from header and extract the data needed to be added
-    secret_key = 'YourSecretKeyHere'
-    token = request.headers.get('Authorization')  # Extract token from header
-    token_expired = is_token_expired(token, secret_key)
-    return jsonify({'message': 'Token verification successful', 'token_expired': token_expired}), 200
+# Endpoint for user logout (no changes)
 
 if __name__ == '__main__':
     app.run(debug=True)
